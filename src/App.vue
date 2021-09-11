7b65e0ff0eb1882d00fdcdd68ac2253c709c74da
macklark <ganesh.kolavennu@gmail.com>
2021-09-11 14:16:12 +0530

    init commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Hello, world!!",
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  text-align: center;
+}
+</style>
