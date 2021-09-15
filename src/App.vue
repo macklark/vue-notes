8483254a91f801cd6e758a6f3f9d723af4166501
macklark <ganesh.kolavennu@gmail.com>
2021-09-15 10:27:54 +0530

    init commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1 class="container" ref="head">CLI basics</h1>
+  <!-- ref attribute should be defined for a element to get handle on it -->
+  <button class="container" @click="clickHandler">Click me</button>
+
+  <!-- Self closing component without children -->
+  <!-- We can pass data from one component to another using props -->
+  <!-- Here carrier is a prop which takes the string 'message from parent to child !' and passes it to Firstcomponent -->
+  <div v-if="toggleState">
+    <!-- <Firstcomponent
+      carrier="message from parent to child !"
+      :object="{ id: 0, name: 'ralph', title: 'student' }"
+      @close="toggleHandler"
+    /> -->
+    <Firstcomponent
+      carrier="message from parent to child !"
+      :object="{ id: 0, name: 'ralph', title: 'student' }"
+      @close="toggleHandler"
+    >
+      <p>This is a child component</p>
+      <p>This is another child component</p>
+    </Firstcomponent>
+    <!-- Getting the custom event and passing toggleHandler function -->
+  </div>
+  <button @click="toggleHandler">Click</button>
+</template>
+
+<script>
+// refs are object method used to get a handle on a DOM element.
+
+// Importing component
+import Firstcomponent from "./components/Firstcomponent.vue";
+export default {
+  name: "App",
+
+  // Need to mention the component to be used in the template inside components object.
+  components: {
+    Firstcomponent,
+  },
+  data() {
+    return {
+      message: "Hello Vite CLI",
+      toggleState: false,
+    };
+  },
+  methods: {
+    clickHandler() {
+      // Then we can use normal javascript functionalities of DOM manipulation.
+      // Such as adding className, removing className, changing text, etc.
+      console.log(this.$refs.head);
+      this.$refs.head.classList.add("active");
+    },
+
+    toggleHandler() {
+      this.toggleState = !this.toggleState;
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  text-align: center;
+  color: aqua;
+}
+
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/Firstcomponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 @click="toggleHandler">{{ message }}</h1>
+  <h1>{{ carrier }}</h1>
+  <p>{{ object.id }}</p>
+  <p>{{ object.name }}</p>
+  <p>{{ object.title }}</p>
+
+  <slot></slot>
+  <!-- Slots are used to pass template rather than just data from one component to another component -->
+</template>
+
+<script>
+export default {
+  name: "Firstcomponent",
+
+  // Mentioning what data to be used in the component from prop.
+  props: ["carrier", "object"],
+  data() {
+    return {
+      message: "Component from another file",
+    };
+  },
+
+  // creating a custom event to pass data to the parent component.
+  methods: {
+    toggleHandler() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<!-- scoped option let's us concentrate particular styles to particular component. -->
+<style scoped>
+h1 {
+  color: blueviolet;
+}
+</style>
